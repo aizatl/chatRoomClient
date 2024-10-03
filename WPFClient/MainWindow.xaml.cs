382cff37d0e79b8bb9602f7ecd139f719c69ea80
Maiza <maizat.321@gmail.com>
2024-10-03 22:16:59 +0800

    cxcx

--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -12,23 +12,41 @@ namespace WPFClient
     public partial class MainWindow : Window
     {
         private NetworkStream stream;
-
+        private string clientName = string.Empty;
         public MainWindow()
         {
             InitializeComponent();
-            ConnectWithServer();
+            UserInput.IsEnabled = false;
+            SendButton.IsEnabled = false;
+            ExitButton.IsEnabled = false;
         }
-
+        private void OkayNameBtnSumbit(object sender, RoutedEventArgs e)
+        {
+            clientName = clientNameTxt.Text.ToString();
+            if (!string.IsNullOrEmpty(clientName))
+            {
+                errorMessage.Visibility = Visibility.Collapsed;
+                clientNameTxt.IsEnabled = false;
+                okayNameBtn.IsEnabled = false;
+                ConnectWithServer();
+                SetName(clientName);
+            }
+            else {
+                errorMessage.Visibility = Visibility.Visible;
+                clientNameTxt.Focus();
+            }
+        }
         private void ConnectWithServer()
         {
             try
             {
-                TcpClient client = new TcpClient("127.0.0.1", 12345);
+                TcpClient client = new TcpClient("192.168.100.11", 12345);// laptop ipconfig
                 stream = client.GetStream();
-
-                // Start the thread to read from the server
                 Thread readThread = new Thread(ReadFromServer);
                 readThread.Start();
+                UserInput.IsEnabled = true;
+                SendButton.IsEnabled = true;
+                ExitButton.IsEnabled = true;
             }
             catch (Exception ex)
             {
@@ -95,6 +113,11 @@ namespace WPFClient
             }
             UserInput.Clear();
         }
+        private void SetName(string name)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(name);
+            stream.Write(data, 0, data.Length);//send name to server
+        }
 
         private void AddMessageToChat(string message, bool isClient, bool justConnect = false)
         {
@@ -122,7 +145,7 @@ namespace WPFClient
                     Margin = new Thickness(5),
                     TextWrapping = TextWrapping.Wrap,
                     Background = new SolidColorBrush(Colors.LightBlue),
-                    HorizontalAlignment = isClient ? HorizontalAlignment.Left : HorizontalAlignment.Right,
+                    HorizontalAlignment = isClient ? HorizontalAlignment.Right : HorizontalAlignment.Left,
                     Padding = new Thickness(1),
                     MaxWidth = 300
                 };
